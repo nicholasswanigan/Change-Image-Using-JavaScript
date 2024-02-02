db1bd7622eccaecf2dfc495a8d9af0009090c1c2
nicholasswanigan <nickswanigan04@gmail.com>
2024-02-02 11:29:38 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function changeImage(thumbnail) {
+    //getting the img information and setting it to thumbnail
+    
+    var mainImg = document.getElementById("mainImg"); // selecting the main image element
+    mainimg.src = thumbnail.src; //set the main img to the previously given thembnail or this element 
+  }
